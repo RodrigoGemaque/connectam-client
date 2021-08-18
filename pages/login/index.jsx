ad2c36cf558e66d80768df424ab07ec057b43b30
Rodrigo <ro.gemaque16@gmail.com>
2021-08-18 10:22:16 -0300

    page login

---
+++ pages/login/index.jsx
@@ -0,0 +1,39 @@
+import { Row, Col, Card } from "react-bootstrap"
+import Header from "../../components/shared/Header"
+// import styles from './styles.module.css'
+import LoginForm from '../../components/shared/LoginForm'
+import SignUpForm from "../../components/shared/SignUpForm"
+
+const LoginPage = () => {
+  return (
+
+    <>
+      <Row className='justify-content-md-center mt-4'>
+
+        <Col md={{ span: 6 }}>
+          <Card className='p-5 mb-3'>
+            <LoginForm
+              titlePhrase="Acessar minha Conta"
+              buttonPhrase="Acessar"
+
+            />
+          </Card>
+        </Col>
+      </Row>
+
+
+      <Row className='justify-content-md-center mt-4'>
+
+        <Col md={{ span: 6 }}>
+          <Card className='p-5 mb-3'>
+            <SignUpForm
+              titlePhrase="Criar nova conta"
+              buttonPhrase="CRIAR" />
+          </Card>
+        </Col>
+      </Row>
+    </>
+  )
+}
+
+export default LoginPage
