f215a58fed042b0d74b1dff24130c6e21af672c3
Rodrigo <ro.gemaque16@gmail.com>
2021-08-21 10:00:15 -0300

    simple check profile test in passengers page

--- pages/passagers/index.jsx
+++ pages/passagers/index.jsx
@@ -2,18 +2,30 @@ import TicketForm from '../../components/shared/NewTicket/TicketForm'
 import { Card, Row, Col } from 'react-bootstrap'
 import withAuth from '../../components/withAuth'
 
+import { useRouter } from 'next/router'
+import { useSelector } from 'react-redux'
 const PassagersForm = () => {
-  return (
-    <>
-      <Row className='justify-content-md-center'>
+const user = useSelector(state => state.auth.loggedUser)
+const router = useRouter()
+  try {
+    if(user.profile === 'ship_owner' || user.profile === 'client' || user.profile === 'intermediary' || user.profile === 'admin'){
+      return (
+        <>
+          <Row className='justify-content-md-center'>
+    
+            <Col md={{ span: 4 }}>
+                <TicketForm />
+            </Col>
+          </Row>
+        </>
+    
+      )
+    }
+  } catch (error) {
+    router.push('/login')
+  }  
 
-        <Col md={{ span: 4 }}>
-            <TicketForm />
-        </Col>
-      </Row>
-    </>
-
-  )
+ 
 }
 
 export default withAuth(PassagersForm);
